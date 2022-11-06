d43012352eaece99d21627866ecb8b7bf724f0f6
ImanG <igeghellache@gmail.com>
2022-11-06 16:18:43 +0000

    added tests and a few tweaks

--- test.js
+++ test.js
@@ -1,3 +1,95 @@
+test("Should add taskinput to list", () => { //why is this failing?
+  // get task input
+  const taskInput = document.querySelector("input[name='task']");
+
+  // enter test data into inputs
+  taskInput.value = "drink";
+
+  // submit the form
+  const submitButton = document.querySelector("button[id='push']");
+  submitButton.click();
+
+  // verify that the page contains the expected result
+  let result = document.querySelector("#tasks");
+  
+  equal(result.textContent, taskInput.value);
+
+ 
+});
+
+//List contains new item
+test('when add button is clicked, display task in the div id=tasks',()=>{
+  const button = document.getElementById('push');
+  button.click();
+  const mytask = document.getElementById('tasks');
+  if (document.body.contains(mytask)) {
+    console.info(`Pass`);
+  } else {
+    console.error(`Fail`);
+  }
+});
+
+test("if input field is empty, display error", () => {
+  document.querySelector('#push').onclick = function(){
+    if(document.querySelector('#newtask input').value.length == 0){
+        alert("Please Enter a Task")
+        console.info(`Pass`)
+    }
+      //it goes to the bottom?
+      // ask about catch and try and assert
+  }});
+
+
+//don't know why this one isn't working-goes to the bottom?
+test("Checking an entry marks it as complete", () => {
+  var current_tasks = document.querySelectorAll(".done");
+  for(var i=0; i<current_tasks.length; i++){
+      current_tasks[i].onclick = function(){
+          this.parentNode.style.textDecoration ="line-through";
+          console.info(`Pass`);
+      }
+  }});
+
+test("Deleting an entry removes it from the list", () => {
+  var current_tasks = document.querySelectorAll(".delete");
+        for(var i=0; i<current_tasks.length; i++){
+            current_tasks[i].onclick = function(){
+                this.parentNode.remove();
+                console.info(`Pass`);
+            }
+        }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Add tasks to a list so that I can keep track of them
 test("Submitting a new task adds it to the list", () => {
     
